a6049ee5cd7f8616a78b5953a77ce24edf9ab10a
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-08 10:36:08 +0500

    user relations added

--- src/eCommerce.Data/DbContexts/eCommerceDbContext.cs
+++ src/eCommerce.Data/DbContexts/eCommerceDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eCommerce.Domain.Entities.Orders;
+using eCommerce.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Data.DbContexts
 {
@@ -8,6 +10,37 @@ namespace eCommerce.Data.DbContexts
             : base(options)
         {
             Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region User relations
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Address)
+                .WithOne(a => a.User)
+                .HasForeignKey<User>(u => u.AddressId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Orders)
+                .WithOne(o => o.User)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Payments)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.CreditCard)
+                .WithOne(c => c.User)
+                .HasForeignKey<User>(u => u.CreditCardId)
+                .OnDelete(DeleteBehavior.NoAction);
+            #endregion
+
+
         }
     }
 }
