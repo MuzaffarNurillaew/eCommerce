b943f239d573e0093f1abe97cbd81838ab783657
behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-04-08 17:47:03 +0500

    Repository implemented.

--- src/eCommerce.Data/Repositories/Repository.cs
+++ src/eCommerce.Data/Repositories/Repository.cs
@@ -1,32 +1,9 @@
 ﻿using eCommerce.Data.IRepositories;
 using eCommerce.Domain.Commons;
+
 namespace eCommerce.Data.Repositories
 {
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
+    public class Repository<T> : IRepository<T> where T : Auditable
     {
-        public Task<bool> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<TEntity> GetAllAsync(Predicate<TEntity> predicate = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> GetByAsync(Predicate<TEntity> predicate = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> InsertAsync(TEntity teintity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> UpdateAsync(TEntity teintity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
