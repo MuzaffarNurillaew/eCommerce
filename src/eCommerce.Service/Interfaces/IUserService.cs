97ea869ab485e5daf2f75eb3fed09a10ace46f16
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-09 12:41:36 +0500

    Some changes in dtos

---
+++ src/eCommerce.Service/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.Domain.Configurations;
+using eCommerce.Domain.Entities.Users;
+using eCommerce.Service.Dtos.Users;
+using System.Linq.Expressions;
+
+namespace eCommerce.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto> CreateAsync(UserCreationDto userDto);
+        Task<UserDto> UpdateAsync(Expression<Func<User, bool>> expression, UserDto userDto);
+        Task<bool> DeleteAsync(Expression<Func<User, bool>> expression);
+        Task<UserDto> GetAsync(Expression<Func<User, bool>> expression);
+        Task<List<UserDto>> GetAllAsync(PaginationParams @params, Expression<Func<User, bool>> expression = null);
+    }
+}
