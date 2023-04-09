aa99fda4069a3b91e642e551ff86eb19077fed43
AbdulloMic <84926707+kerakmas@users.noreply.github.com>
2023-04-09 18:36:47 +0500

    some views added

---
+++ src/eCommerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+    }
+}
