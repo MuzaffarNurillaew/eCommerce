e5a4855d1e424b97680152e52c710ad2d844355f
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-10 12:18:26 +0500

    new migration, searchtag and productsearchtag services implemented

---
+++ src/eCommerce.Data/Migrations/20230410065800_newPropertiesInEntities.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Data.Migrations
+{
+    public partial class newPropertiesInEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Count",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Count",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
