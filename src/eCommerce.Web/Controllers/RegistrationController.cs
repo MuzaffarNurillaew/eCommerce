7e87ef223693264e72a1120074c2aabd8301869b
AbdulloMic <84926707+kerakmas@users.noreply.github.com>
2023-04-10 13:47:01 +0500

    Page changes

--- src/eCommerce.Web/Controllers/RegistrationController.cs
+++ src/eCommerce.Web/Controllers/RegistrationController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Service.Dtos.Users;
+using eCommerce.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Web.Controllers
 {
     public class RegistrationController : Controller
     {
+        private readonly IUserService userService;
+
+
         public IActionResult SignUp()
         {
             return View();
+        }
+        public async Task<IActionResult> DoSignUp(UserCreationDto userCreationDto) 
+        { 
+            var service = await this.userService.CreateAsync(userCreationDto);
+            return View("Index", "Product");
         }
         public IActionResult SignIn() 
         { 
